cead3111dc6e0861aa0da20feebe4adf39877abe
Pavel <P.A.Pisarev@yandex.ru>
2023-03-03 14:25:06 +0600

    gradle and security updates

---
+++ service-api-gateway/src/main/java/org/example/gateway/configuration/SecurityConfig.java
@@ -0,0 +1,20 @@
+package org.example.gateway.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityWebFilterChain oAuth2Config(ServerHttpSecurity http) {
+        http
+            .authorizeExchange().anyExchange().authenticated()
+            .and()
+			.oauth2Login();
+        return http.build();
+    }
+}
